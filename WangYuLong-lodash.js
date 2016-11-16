b9c25bc751f612478780f4264b209dbf66c84f35
wangyulong <515958725@qq.com>
2016-11-16 21:48:40 +0800

    second

--- WangYuLong-lodash.js
+++ WangYuLong-lodash.js
@@ -33,4 +33,45 @@ var WangYuLong = {
         }
         return newArr
     },
+    difference: function() {
+        var newArr = []
+        for (var i = 0; i < arguments[0].length; i++) {
+            newArr.push(arguments[0][i])
+        }
+        for (var j = 0; j < newArr.length; j++) {
+            for (var k = 1; k < arguments.length; k++) {
+                for (var m = 0; m < arguments[k].length; m++) {
+                    if (newArr[j] == arguments[k][m]) {
+                        newArr.splice(j, 1)
+                    }
+                }
+            }
+        }
+        return newArr
+    },
+    drop: function() {
+        var newArr = []
+        for (var i = 0; i < arguments[0].length; i++) {
+            newArr.push(arguments[0][i])
+        }
+        if (arguments[1] == undefined) {
+            arguments[1] = 1
+        }
+        newArr.splice(0, arguments[1])
+        return newArr
+    },
+    dropRight: function() {
+        var newArr = []
+        for (var i = 0; i < arguments[0].length; i++) {
+            newArr.push(arguments[0][i])
+        }
+        if (arguments[1] == undefined) {
+            arguments[1] = 1
+        } else if (arguments[1] > newArr.length) {
+            arguments[1] = newArr.length
+        }
+        newArr.splice(newArr.length - arguments[1], arguments[1])
+        return newArr
+    },
+
 }
