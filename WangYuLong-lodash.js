02c4b36ad0f6bc81366b5833a1dc6886178f2e55
wangyulong <515958725@qq.com>
2016-12-01 23:10:38 +0800

    a

--- WangYuLong-lodash.js
+++ WangYuLong-lodash.js
@@ -1734,4 +1734,114 @@ var WangYuLong = {
         }
         return valueArr
     },
+    parseJson: function(str) {
+        var json = str
+        var i = 0
+        return parse()
+
+        function parse() {
+            if (!isNaN(json[i])) {
+                return parseNumber()
+            }
+            if (json[i] === '"') {
+                return parseString()
+            }
+            if (json[i] === 't') {
+                return parseTrue()
+            }
+            if (json[i] === 'f') {
+                return parseFalse()
+            }
+            if (json[i] === 'n') {
+                return parseNull()
+            }
+            if (json[i] === '[') {
+                return parseArray()
+            }
+            if (json[i] === '{') {
+                return parseObject()
+            }
+        }
+
+        function parseNumber() {
+            var start = i
+            while (true) {
+                i++
+                if (isNaN(json[i])) {
+                    return Number(json.slice(start, i))
+                }
+            }
+        }
+
+        function parseString() {
+            var start = i + 1
+            var index = json.indexOf('"', start)
+            var string = json.slice(start, index)
+            i = index + 1
+            return string
+        }
+
+        function parseTrue() {
+            i = i + 4
+            return true
+        }
+
+        function parseFalse() {
+            i = i + 5
+            return false
+        }
+
+        function parseNull() {
+            i = i + 4
+            return null
+        }
+
+        function parseArray() {
+            var result = []
+            i++
+            while (true) {
+                if (json[i] === ']') {
+                    i++
+                    break
+                }
+                result.push(parse())
+                if (json[i] == ',') {
+                    i++
+                }
+            }
+            return result
+        }
+
+        function parseObject() {
+            var result = {}
+            i++
+            var key
+            var value
+            while (true) {
+                if (json[i] == '}') {
+                    break
+                }
+                key = parseString()
+                i++
+                value = parse()
+                result[key] = value
+                if (json[i] == ',') {
+                    i++
+                    continue
+                }
+            }
+            i++
+            return result
+        }
+    },
+    forOwn: function(obj, func) {
+        for (var key in obj) {
+            if (Object.prototype.hasOwnProperty.call(obj, key)) {
+                if (func(obj[key], key, obj) === false) {
+                    return obj
+                }
+            }
+        }
+        return obj
+    },
 }
